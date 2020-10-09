51dd65fbe1500f854241e891ceda8687a148daf5
fahmi277 <fahmi.sangengon@gmail.com>
2020-10-09 12:56:22 +0700

    get data from rest api

--- lib/service/apiService.dart
+++ lib/service/apiService.dart
@@ -8,17 +8,21 @@ import 'package:hinergi_kwh/service/apiKey.dart';
 // }
 
 Future<Map> getThinkspeakData() async {
-  var dio = Dio();
+  try {
+    var dio = Dio();
 
-  String channelId = dataApi().channelID;
-  String apiKey = dataApi().apiKey;
-  String baseUrl = "https://api.thingspeak.com/channels/";
-  String feedUrl = "/feeds/last.json?api_key=";
-  String timezoneUrl = "&timezone=Asia%2FJakarta";
+    String channelId = dataApi().channelID;
+    String apiKey = dataApi().apiKey;
+    String baseUrl = "https://api.thingspeak.com/channels/";
+    String feedUrl = "/feeds/last.json?api_key=";
+    String timezoneUrl = "&timezone=Asia%2FJakarta";
 
-  String url = baseUrl + channelId + feedUrl + apiKey + timezoneUrl;
-  Response response = await dio.get(url);
-  print(response.data);
+    String url = baseUrl + channelId + feedUrl + apiKey + timezoneUrl;
+    Response response = await dio.get(url);
+    print(response.data);
 
-  return response.data;
+    return response.data;
+  } catch (e) {
+    return null;
+  }
 }
--- lib/view/mainScreen.dart
+++ lib/view/mainScreen.dart
@@ -16,50 +16,75 @@ class _mainScreenState extends State<mainScreen> {
         appBar: AppBar(
           title: Text("Hinergi"),
         ),
-        body: Stack(
-          children: <Widget>[
-            StreamBuilder(
-                stream: Stream.periodic(Duration(seconds: 5)).asyncMap((i) =>
-                    getThinkspeakData()), // i is null here (check periodic docs)
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(
-                      "Data : " + snapshot.data['field1'].toString(),
-                      style: TextStyle(color: Colors.green),
-                    );
-                  } else {
-                    return Text(
-                      "Data : NULL",
-                      style: TextStyle(color: Colors.red),
-                    );
-                  }
-                } // builder should also handle the case when data is not fetched yet
-                ),
-            Center(
-                child: SfRadialGauge(
-              axes: <RadialAxis>[
-                RadialAxis(annotations: <GaugeAnnotation>[
-                  GaugeAnnotation(
-                      axisValue: 50,
-                      positionFactor: 0.1,
-                      widget: Text(
-                        '30.0',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ))
-                ], pointers: <GaugePointer>[
-                  MarkerPointer(
-                      value: 60,
-                      color: Colors.red,
-                      markerType: MarkerType.text,
-                      text: "BUDGET"),
-                  RangePointer(
-                      value: 30, color: Colors.green, dashArray: <double>[8, 2])
-                ])
-              ],
-            )),
-          ],
-        ),
+        body: StreamBuilder(
+            stream: Stream.periodic(Duration(seconds: 5)).asyncMap((i) =>
+                getThinkspeakData()), // i is null here (check periodic docs)
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                double voltage = double.parse(snapshot.data['field1']);
+                double energy = double.parse(snapshot.data['field4']);
+
+                energy -= 772.3;
+                voltage /= 10;
+                return Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Card(
+                          child: ListTile(
+                            title: Text(snapshot.data['created_at']),
+                            subtitle: Text(snapshot.data['created_at']),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Center(
+                        child: SfRadialGauge(
+                      axes: <RadialAxis>[
+                        RadialAxis(
+                            startAngle: 180,
+                            endAngle: 90,
+                            annotations: <GaugeAnnotation>[
+                              GaugeAnnotation(
+                                  axisValue: 50,
+                                  positionFactor: 0.1,
+                                  widget: Text(
+                                    energy.toStringAsFixed(2).toString() +
+                                        " KWH",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ))
+                            ],
+                            pointers: <GaugePointer>[
+                              MarkerPointer(
+                                  value: 60,
+                                  color: Colors.red,
+                                  markerType: MarkerType.text,
+                                  text: "BUDGET"),
+                              RangePointer(
+                                  value:
+                                      double.parse((energy.toStringAsFixed(0))),
+                                  color: Colors.green,
+                                  dashArray: <double>[8, 2])
+                            ])
+                      ],
+                    )),
+                  ],
+                );
+                // return Text(
+                //   "Data : " + snapshot.data['field1'].toString(),
+                //   style: TextStyle(color: Colors.green),
+                // );
+              } else {
+                return Text(
+                  "Data : NULL",
+                  style: TextStyle(color: Colors.red),
+                );
+              }
+            } // builder should also handle the case when data is not fetched yet
+            ),
       ),
     );
   }
