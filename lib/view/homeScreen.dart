71502a18b6e8a44c403b35e4d1ca564df66874c7
Agung <agungresistorc828@gmail.com>
2020-10-10 11:49:33 +0700

    add home screen and setting screen

--- lib/view/homeScreen.dart
+++ lib/view/homeScreen.dart
@@ -1,81 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:hinergi_kwh/service/apiService.dart';
+import 'package:loading/indicator/ball_pulse_indicator.dart';
+import 'package:loading/loading.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:intl/intl.dart';
 
 class homeScreen extends StatelessWidget {
+  Color _colorGauge = Colors.green;
+  double maxKwh = 3; // budget satuan budget kwh
+  double limitKwh = 3 * 0.7; //limit warning kwh
 
   homeScreen();
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-            stream: Stream.periodic(Duration(seconds: 5)).asyncMap((i) =>
-                getThinkspeakData()), // i is null here (check periodic docs)
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                double voltage = double.parse(snapshot.data['field1']);
-                double energy = double.parse(snapshot.data['field4']);
+              stream: Stream.periodic(Duration(seconds: 5)).asyncMap((i) =>
+                  getThinkspeakData()), // i is null here (check periodic docs)
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var dataMasuk = snapshot.data["feeds"];
+                  List lengthDataToday = snapshot.data["feeds"];
+                  double startKwhToday = double.parse(dataMasuk[0]["field4"]);
+                  double lastKwhToday = double.parse(
+                      dataMasuk[lengthDataToday.length - 1]["field4"]);
 
-                energy -= 772.3;
-                voltage /= 10;
-                return Stack(
-                  children: [
-                    // Column(
-                    //   mainAxisAlignment: MainAxisAlignment.end,
-                    //   children: [
-                    //     Card(
-                    //       child: ListTile(
-                    //         title: Text(snapshot.data['created_at']),
-                    //         subtitle: Text(snapshot.data['created_at']),
-                    //       ),
-                    //     ),
-                    //   ],
-                    // ),
-                    Center(
-                        child: SfRadialGauge(
-                      axes: <RadialAxis>[
-                        RadialAxis(
-                            startAngle: 180,
-                            endAngle: 0,
-                            annotations: <GaugeAnnotation>[
-                              GaugeAnnotation(
-                                  axisValue: 50,
-                                  positionFactor: 0.1,
-                                  widget: Text(
-                                    energy.toStringAsFixed(2).toString() +
-                                        " KWH",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20),
-                                  ))
-                            ],
-                            pointers: <GaugePointer>[
-                              MarkerPointer(
-                                  value: 60,
-                                  color: Colors.red,
-                                  markerType: MarkerType.text,
-                                  text: "BUDGET"),
-                              RangePointer(
-                                  value:
-                                      double.parse((energy.toStringAsFixed(0))),
-                                  color: Colors.green,
-                                  dashArray: <double>[8, 2])
-                            ])
-                      ],
-                    )),
-                  ],
-                );
-                // return Text(
-                //   "Data : " + snapshot.data['field1'].toString(),
-                //   style: TextStyle(color: Colors.green),
-                // );
-              } else {
-                return Text(
-                  "Data : NULL",
-                  style: TextStyle(color: Colors.red),
-                );
+                  if (startKwhToday > lastKwhToday) {
+                    startKwhToday = 0;
+                  }
+                  double kwhNow = lastKwhToday - startKwhToday;
+
+                  String tempTimeStartToday = dataMasuk[0]["created_at"]
+                      .toString()
+                      .split("+")[0]
+                      .replaceAll("T", " ");
+                  String tempTimeLastToday =
+                      dataMasuk[lengthDataToday.length - 1]["created_at"]
+                          .toString()
+                          .split("+")[0]
+                          .replaceAll("T", " ");
+
+                  DateTime timeStartToday =
+                      new DateFormat("yyyy-MM-dd hh:mm:ss")
+                          .parse(tempTimeStartToday);
+                  DateTime timeLastToday = new DateFormat("yyyy-MM-dd hh:mm:ss")
+                      .parse(tempTimeLastToday);
+
+                  // change color
+                  double warning = limitKwh * 0.5;
+                  if (kwhNow >= warning) {
+                    _colorGauge = Colors.red;
+                  } else {
+                    _colorGauge = Colors.green;
+                  }
+
+                  return Stack(
+                    children: [
+                      Card(
+                        // child: Text(snapshot.data['created_at']),
+                        child: ListTile(
+                          title: Text("Start : " +
+                              timeStartToday.toString().split(".")[0] +
+                              " KWH : " +
+                              startKwhToday.toStringAsFixed(1)),
+                          subtitle: Text("Last : " +
+                              timeLastToday.toString().split(".")[0] +
+                              " KWH : " +
+                              lastKwhToday.toStringAsFixed(1)),
+                          // subtitle: Text(snapshot.data['created_at']),
+                        ),
+                      ),
+                      // Card(
+                      //   child: Text(snapshot.data['created_at']),
+                      //   // child: ListTile(
+                      //   //   title: Text(snapshot.data['created_at']),
+                      //   //   // subtitle: Text(snapshot.data['created_at']),
+                      //   // ),
+                      // ),
+                      Center(
+                          child: SfRadialGauge(
+                        axes: <RadialAxis>[
+                          RadialAxis(
+                              minimum: 0,
+                              maximum: maxKwh,
+                              startAngle: 120,
+                              endAngle: -30,
+                              annotations: <GaugeAnnotation>[
+                                GaugeAnnotation(
+                                    axisValue: 50,
+                                    positionFactor: 0.1,
+                                    widget: Text(
+                                      kwhNow.toStringAsFixed(2) + " KWH",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ))
+                              ],
+                              pointers: <GaugePointer>[
+                                MarkerPointer(
+                                    value: limitKwh,
+                                    color: Colors.red,
+                                    markerType: MarkerType.text,
+                                    text: "BUDGET"),
+                                RangePointer(
+                                    value: kwhNow,
+                                    // double.parse(
+                                    //     (energy.toStringAsFixed(0))),
+                                    color: _colorGauge,
+                                    dashArray: <double>[8, 2])
+                              ])
+                        ],
+                      )),
+                    ],
+                  );
+                  // return Text(
+                  //   "Data : " + snapshot.data['field1'].toString(),
+                  //   style: TextStyle(color: Colors.green),
+                  // );
+                } else {
+                  return Container(
+                    // color: Colors.lightBlue,
+                    child: Center(
+                      child: Loading(
+                          indicator: BallPulseIndicator(),
+                          size: 100.0,
+                          color: Colors.lightBlue),
+                    ),
+                  );
+                }
               }
-            } // builder should also handle the case when data is not fetched yet
-            );
+              // builder should also handle the case when data is not fetched yet
+              );
   }
 }
---
+++ lib/view/settingScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class settingScreen extends StatelessWidget {
+
+  settingScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    // return Container(MyTextFormField(
+    //         hintText: 'Email',
+    //         isEmail: true,
+    //         validator: (String value) {
+    //           if (!validator.isEmail(value)) {
+    //             return 'Please enter a valid email';
+    //           }
+    //           return null;
+    //         },
+    //         onSaved: (String value) {
+    //           model.email = value;
+    //         },
+    //       ),)
+
+  }
+}
